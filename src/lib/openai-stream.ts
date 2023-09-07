f4ad5dba4bd54f19394335d95858c8f7c5a832f2
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-07 16:52:07 +0530

    streaming the api route

--- src/lib/openai-stream.ts
+++ src/lib/openai-stream.ts
@@ -1,6 +1,21 @@
+import { boolean, number, string } from "zod";
+
 export type ChatGPTAgent = "user" | "system";
 
 export interface ChatGPTMessage {
   role: ChatGPTAgent;
   content: string
 }
+
+export interface OpenAIStreamPayload {
+    model: string
+    messages: ChatGPTMessage[]
+    tempurature: number
+    top_p: number
+    frequency_penalty: number
+    presence_panalty: number
+    max_tokens: number
+    stream: boolean
+    n: number
+  }
+
