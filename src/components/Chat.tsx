f661971d31b890d7222c4cff525ad252ec2de561
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-15 16:07:24 +0530

    adding chat messages

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -14,7 +14,7 @@ const Chat: FC = () => {
           </AccordionTrigger>
           <AccordionContent>
             <div className='flex flex-col h-80'>
-              <ChatMessages /> 
+              <ChatMessages className='px-2 py-3 flex-1'/> 
               <ChatInput className='px-4' /> 
             </div>
           </AccordionContent>
--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -1,10 +1,24 @@
-import {FC} from 'react'
+"use client"
+import { MessagesContext } from '@/context/messages';
+import {FC, HTMLAttributes, useContext} from 'react'
+import {cn} from "@/lib/utils";
 
-interface ChatMessagesProps {}
+interface ChatMessagesProps extends HTMLAttributes<HTMLDivElement> {}
 
-const ChatMessages: FC<ChatMessagesProps> = ({}) => {
+const ChatMessages: FC<ChatMessagesProps> = ({className, ...props}) => {
+  const {messages} = useContext(MessagesContext);
+  const inverseMessages = [...messages].reverse();
   return (
-    <div>ChatMessages</div>
+    <div {...props} className={cn(
+      'flex flex-col-reverse gap-3 overflow-y-auto scrollbar-thumb-blue scroll-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch', 
+      className
+    )}>
+      <div className='flex-1 flex-grow'/>
+      {inverseMessages.map((message) => (
+      <div key={message.id} className='chat-message'></div>
+      ))}
+      ChatMessages
+    </div>
   )
 }
 
